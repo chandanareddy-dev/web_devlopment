bf56d2a39d704d83b455f17e06d597549b222aa4
chandanareddy-dev <82564172+chandanareddy-dev@users.noreply.github.com>
2021-05-06 20:55:51 +0530

    Create script.js

---
+++ sandbox/day_10_javascript/js/script.js
@@ -0,0 +1,43 @@
+//alert("hello from script")
+
+//document.write("document data");
+
+
+//method
+console.log("hello console");
+
+
+//datatypes
+
+var names = "priya"; //string
+var firstName="reddy";//string
+var firstname="riya"; //string
+var phone = 5262726827; //int
+var isDev = true; //boolean
+//var a = NULL; //NULL
+var place; //undefine
+console.log("full name=" + firstName + " "+"phone number is"+phone +".am i devloper,"+isDev);//adding two data types(concatinating)
+//console.log(names);
+//console.log(phone);
+//console.log(isDev);
+//console.log(place);
+
+
+
+//operators
+
+//+
+//=
+//-
+//*
+// /
+var a=4;
+var b=5;
+var total=a+b;
+var discount=2;
+var total=a + b - discount;
+var total = a * b / discount;
+//console.log(total);
+console.log(a + " +" + b+ "=" + total);//joining the data
+console.log(a + " +" + b+ "=" + total + "(discount " + discount + ")");
+
